dce4c72814971bf00d17c34b2b5837faa19d7ee0
eliam lotonga <eliam lotonga>
2017-12-11 15:54:16 +0100

    Create first class and first Component

--- amap/src/index.js
+++ amap/src/index.js
@@ -1,1 +1,14 @@
 // c'est parti !
+import React from 'react';
+import { render } from 'react-dom';
+class StorePicker extends React.Component{
+
+	render(){
+		return (<div className = "content">
+					<p>Hello !</p>
+				</div>)
+			
+	}
+}
+
+render(<StorePicker/>,document.querySelector("#main") );
